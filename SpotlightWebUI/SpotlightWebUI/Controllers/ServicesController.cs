3d010efdc08f1b7f3b385f8ed3b138b51359c039
Angad Dhawan <angad.dhawan@inshapeclubs.com>
2018-01-19 13:02:12 -0800

    .

---
+++ SpotlightWebUI/SpotlightWebUI/Controllers/ServicesController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace SpotlightWebUI.Controllers {
+    public class ServicesController : Controller
+    {
+        // GET: Services
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
